8bda869f4261cb56e0d2d7b6d82a1ab606f8a910
jkvaland17 <108795457+jkvaland17@users.noreply.github.com>
2022-08-10 16:41:47 +0530

    55

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,9 @@
+import React from "react";
 function Card(props) {
   return (
     <div className="container">
       <div className="card">
-        <img src={props.imgsrc} alt="Avatar"></img>
+        <img src={props.imgsrc} alt="Avatar" />
         <h2>{props.titel}</h2>
         <h3>{props.snma}</h3>
         <a href={props.link} target="_blank" rel="noreferrer">
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import "./index.css";
 import Card from "./Card";
 import Data from "./Data";
@@ -15,24 +13,12 @@ let ncard = (val) => {
       snma={val.snma}
       link={val.link}
     />
-  )
-}
-
-// function ncard(val) {
-//   return (
-//     <Card
-//       imgsrc={val.imgsrc}
-//       titel={val.titel}
-//       snma={val.snma}
-//       link={val.link}
-//     />
-//   );
-// }
+  );
+};
 
 ReactDOM.render(
   <>
-    <h1 className="titel">Netflix Best Series 2022</h1>
-    {Data.map(ncard)};
+    <h1 className="titel">Netflix Best Series 2022</h1>,{Data.map(ncard)};
   </>,
   document.getElementById("root")
 );
